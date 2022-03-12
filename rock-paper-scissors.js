11cfbcb015cbb35c6d64326f14f7d08507f3b828
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-03-12 07:54:48 -0500

    Add function for computer's move

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,2 +1,18 @@
 //A console-based version of the classic game Rock, Paper, Scissors
 
+
+//Function to randomly select computer play
+function computerPlay () {
+  let randomChoice = Math.floor(Math.random() * 3);
+  const playOptions = ["Rock", "Paper", "Scissors"];
+
+  return playOptions[randomChoice];
+}
+
+//Function to prompt user for their selection
+
+//Function to play a single round and return the winner of that round
+//This should have two paramters, one for each player's move
+
+//Function for the whole game
+//Game should consist of 5 rounds
