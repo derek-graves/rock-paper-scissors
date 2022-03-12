49d5503c826f02552908fcea91a02806ca4afd04
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-03-12 14:03:25 -0500

    Complete for loop struture and add winner message

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,5 @@
 //A console-based version of the classic game Rock, Paper, Scissors
 
-
 //Function to randomly select computer play
 function computerPlay () {
   let randomChoice = Math.floor(Math.random() * 3);
@@ -9,37 +8,50 @@ function computerPlay () {
   return playOptions[randomChoice];
 }
 
-//Function to prompt user for their selection
-
 //Function to play a single round and return the winner of that round
-//This should have two paramters, one for each player's move
 function playRound (playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
+  let message;
   let output;
 
   if (playerSelection === computerSelection) {
-    output = "It's a tie!";
+    message = "It's a tie!";
+    output = null;
   } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
-    output = `You win! ${playerSelection} beats ${computerSelection}`;
+    message = `You win! ${playerSelection} beats ${computerSelection}`;
+    output = true;
   } else {
-    output = `You lose! ${computerSelection} beats ${playerSelection}`;
+    message = `You lose! ${computerSelection} beats ${playerSelection}`;
+    output = false;
   }
 
+  console.log(message)
   return output;
 }
-console.log(playRound("rock", "paper"))
 
 //Function for the whole game
-//Game should consist of 5 rounds
 function game () {
-  const playerSelectionPrompt = "Your move. You may choose rock, paper, or scissors.";
-  console.log("Welcome to Rock, Paper, Scissors! You'll be playing against the computer in a best-of-5 format.\n");
+  const playerMovePrompt = "Your move. You may choose rock, paper, or scissors.";
+  console.log("Welcome to Rock, Paper, Scissors! You'll be playing against the computer in a five-round format.\n")
   let playerScore = 0;
   let computerScore = 0;
 
-  for (let i = 0, i < 5, i++) {
-
+  for (let i = 0; i < 5; i++) {
+    playerMove = prompt(playerMovePrompt);
+    roundResult = playRound(playerMove, computerPlay());
+    if (!(roundResult == null)) {
+      (roundResult) ? playerScore++ : computerScore++;
+    }
+    console.log(`The current score is    Player: ${playerScore} - Computer: ${computerScore}`)
   }
+
+  let winnerMessage;
+  if (playerScore === computerScore) {
+    winnerMessage = "You're no better than a completely randomized algorithm. You're also no worse."
+  } else {
+    winnerMessage = (playerScore > computerScore) ? "You won! Feel good about yourself." : "You lost to a completely randomized algorithm. No judgement.";
+  }
+  console.log(winnerMessage)
 }
 
-
+game()
