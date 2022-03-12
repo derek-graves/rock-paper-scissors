6cbbdab078ab03bc577e4a224b1ea29ed9f4bbd9
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-03-12 08:21:01 -0500

    Write function to play one round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,7 +4,7 @@
 //Function to randomly select computer play
 function computerPlay () {
   let randomChoice = Math.floor(Math.random() * 3);
-  const playOptions = ["Rock", "Paper", "Scissors"];
+  const playOptions = ["rock", "paper", "scissors"];
 
   return playOptions[randomChoice];
 }
@@ -13,6 +13,19 @@ function computerPlay () {
 
 //Function to play a single round and return the winner of that round
 //This should have two paramters, one for each player's move
+function playRound (computerSelection, playerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  let output;
 
+  if (playerSelection === computerSelection) {
+    output = "It's a tie!";
+  } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+    output = `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    output = `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+
+  return output;
+}
 //Function for the whole game
 //Game should consist of 5 rounds
